dd9cabf6669f6e75dcc29ebb8e5235834d70def2
Devon Terrell <zeugmasyllepsis@gmail.com>
2022-05-29 09:49:42 -0700

    fixup! Implement health check and test

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -9,17 +9,17 @@ async fn health_check_words() {
     let response = client
         .get(format!("{}/health_check", &address))
         .send()
-        .await.expect("Failed to execute request");
+        .await
+        .expect("Failed to execute request");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
-    let _  = tokio::spawn(server);
+    let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
